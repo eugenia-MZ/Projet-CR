79aadc9171394d8f8a4f13258f990c90e58d3b0c
mzitouni <mzitouni@eugeniaschool.com>
2025-10-20 12:49:44 +0000

    scraping several files

--- statsroyale.py
+++ statsroyale.py
@@ -1,12 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get("https://statsroyale.com/fr/deckbuilder")
+response = requests.get("https://cdn.statsroyale.com/gamedata-v4.json")
+result_deck_popular = requests.get("https://stats-royale-api-js-beta-z2msk5bu3q-uk.a.run.app/deckbuilder/search?size=30&source=pathoflegend&sort=count")
 
 content_html = response.text
 print(response.status_code)
 
-with open("result_scrap.html", "w") as file:
+content_deck_popular = result_deck_popular.text
+
+with open("all_data_CR.html", "w") as file:
     file.write(content_html)
 
-soup = BeautifulSoup(content_html, "html.parser")
+with open("deck_popular.html", "w") as file2:
+    file2.write(content_deck_popular)
+
+# soup = BeautifulSoup(content_html, "html.parser")
+soup = BeautifulSoup(content_deck_popular, "html.parser")
+soup.prettify()
