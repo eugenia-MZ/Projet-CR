e92b19d65ed477c4bec0e8cc687165cd9a4307b1
mzitouni <mzitouni@eugeniaschool.com>
2025-10-20 16:52:47 +0000

    recuperation des cartes de chaque catégorie

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ win_condition_response = requests.get("https://www.deckshop.pro/card/flag/win-co
 
 win_condition_html = win_condition_response.text
 
+"""
 with open("win-condition.html", "w") as file:
     file.write(win_condition_html)
-
+"""
 
 for url in all_categories:
     category_response = requests.get(url)
@@ -24,7 +25,20 @@ for url in all_categories:
 
     filename = url.split("/")[-1]
 
+    """
     with open(f"{filename}.html", "w") as file:
         file.write(category_content)
+    """
 
-    soup_anti_air_troop = BeautifulSoup(category_content, "html.parser")
+    soup = BeautifulSoup(category_content, "html.parser")
+
+    links = []
+
+    for img in soup.find_all("img", class_="card"):
+        if "opacity-30" not in img.get("class", []) and "grayscale" not in img.get("class", []):
+            parent_a = img.find_parent("a", href=True)
+            if parent_a:
+                links.append(parent_a["href"])
+
+    print(f"\n📄 Fichier : {filename}")
+    print("\n".join(links) if links else "→ Aucun lien trouvé")
